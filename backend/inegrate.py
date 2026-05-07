43d77371e034666254d6c5984cd88bdcfba5359d
AbhradeepBhattacharyya2007 <bhattacharyya.abhradeep@gmail.com>
2026-05-07 21:11:00 +0530

    Update inegrate.py

--- backend/inegrate.py
+++ backend/inegrate.py
@@ -1,1 +1,49 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login_check', methods=['POST'])
+def login_check():
+
+    username = request.form['username']
+    password = request.form['password']
+
+    conn = sqlite3.connect('traffic.db')
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT role FROM users WHERE username=? AND password=?",
+        (username, password)
+    )
+
+    result = cursor.fetchone()
+
+    conn.close()
+
+    if result:
+
+        role = result[0]
+
+        if role == 'admin':
+            return redirect('/admin')
+
+        else:
+            return redirect('/user')
+
+    return "Invalid Username or Password"
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
