e60dc98e3b5099c17eb50b7b85aa02f797284453
AbhradeepBhattacharyya2007 <bhattacharyya.abhradeep@gmail.com>
2026-05-10 18:49:00 +0530

    Delete backend/database.py

--- backend/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "mysql+pymysql://root:password@localhost/traffic_db"
-
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
